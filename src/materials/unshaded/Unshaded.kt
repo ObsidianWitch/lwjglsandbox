773500d367b65ddf20749080255bb1cf3f2f4484
Obsidienne <obsidienne@protonmail.com>
2017-04-22 00:40:23 +0200

    Materials - unshaded/Unshaded.vs -> general/Main.vs

--- src/materials/unshaded/Unshaded.kt
+++ src/materials/unshaded/Unshaded.kt
@@ -9,7 +9,7 @@ import sandbox.models.Texture
 
 class Unshaded : Material {
     override val shader: Shader = Shader().apply {
-       add(GL_VERTEX_SHADER, "src/materials/unshaded/Unshaded.vs")
+       add(GL_VERTEX_SHADER, "src/materials/general/Main.vs")
        add(GL_FRAGMENT_SHADER, "src/materials/unshaded/Unshaded.fs")
        link()
     }
